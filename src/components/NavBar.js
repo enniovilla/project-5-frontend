6632b1f87cb1b4b6c7911085f4cfd64eb0cd8ca2
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-02 12:48:37 +0000

    Change navlink and add style

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
-      <Navbar.Brand className={styles.logo}>connectify.</Navbar.Brand>
+      <NavLink to="/">
+        <Navbar.Brand className={styles.logo}>connectify.</Navbar.Brand>
+      </NavLink>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="text-center">
-          <Nav.Link className="mx-3">Home</Nav.Link>
-          <Nav.Link className="mx-3">Log in</Nav.Link>
-          <Nav.Link className="mx-3">Sign up</Nav.Link>
+          <NavLink className={styles.NavLink} to="/">
+            Home
+          </NavLink>
+          <NavLink className={styles.NavLink} to="/signin">
+            Log in
+          </NavLink>
+          <NavLink className={styles.NavLink} to="/signup">
+            Sign up
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
