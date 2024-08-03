e49209114b04d9921c4c11e259c1aef749f65729
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 13:25:52 +0000

    Adjust logged in and logged out navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,19 +3,46 @@ import { Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
 
-  const loggedInIcons = <>{currentUser?.username}</>;
+  const loggedInIcons = (
+    <>
+      <NavLink className={styles.NavLink} to="/feed">
+      <i class="fa-solid fa-users fa-xl"></i>Feed
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/events/create">
+      <i class="fa-solid fa-pen-to-square fa-xl"></i>Create Event
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/my_events">
+      <i class="fa-solid fa-calendar fa-xl"></i>My Events
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/my_attendances">
+      <i class="fa-solid fa-clipboard-user fa-xl"></i>My Attendances
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/favorites">
+      <i class="fa-solid fa-star fa-xl"></i>Favorites
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        to={`/profiles/${currentUser?.profile_id}`}
+      ><Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+      <Nav.Link className={styles.NavLink}>
+      <i class="fa-solid fa-right-to-bracket fa-xl"></i>Sign out
+      </Nav.Link>
+    </>
+  );
 
   const loggedOutIcons = (
     <>
       <NavLink className={styles.NavLink} to="/signin">
-        <i class="fa-solid fa-right-to-bracket fa-xl"></i> Sign in
+        <i class="fa-solid fa-right-to-bracket fa-xl"></i>Sign in
       </NavLink>
       <NavLink className={styles.NavLink} to="/signup">
-        <i class="fa-solid fa-user-pen fa-xl"></i> Sign up
+        <i class="fa-solid fa-user-pen fa-xl"></i>Sign up
       </NavLink>
     </>
   );
@@ -29,7 +56,7 @@ const NavBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className=" ml-auto text-center">
           <NavLink className={styles.NavLink} to="/">
-            <i class="fa-solid fa-house fa-xl"></i> Home
+            <i class="fa-solid fa-house fa-xl"></i>Home
           </NavLink>
           {currentUser ? loggedInIcons : loggedOutIcons}
         </Nav>
