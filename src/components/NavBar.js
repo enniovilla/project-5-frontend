a601bb0d391a1684292f73e4cfbbb6f989288098
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 13:34:29 +0000

    Add click outside toggle to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,19 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import {
+  useCurrentUser,
+  useSetCurrentUser,
+} from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -22,27 +28,28 @@ const NavBar = () => {
   const loggedInIcons = (
     <>
       <NavLink className={styles.NavLink} to="/feed">
-      <i class="fa-solid fa-users fa-xl"></i>Feed
+        <i class="fa-solid fa-users fa-xl"></i>Feed
       </NavLink>
       <NavLink className={styles.NavLink} to="/events/create">
-      <i class="fa-solid fa-pen-to-square fa-xl"></i>Create Event
+        <i class="fa-solid fa-pen-to-square fa-xl"></i>Create Event
       </NavLink>
       <NavLink className={styles.NavLink} to="/my_events">
-      <i class="fa-solid fa-calendar fa-xl"></i>My Events
+        <i class="fa-solid fa-calendar fa-xl"></i>My Events
       </NavLink>
       <NavLink className={styles.NavLink} to="/my_attendances">
-      <i class="fa-solid fa-clipboard-user fa-xl"></i>My Attendances
+        <i class="fa-solid fa-clipboard-user fa-xl"></i>My Attendances
       </NavLink>
       <NavLink className={styles.NavLink} to="/favorites">
-      <i class="fa-solid fa-star fa-xl"></i>Favorites
+        <i class="fa-solid fa-star fa-xl"></i>Favorites
       </NavLink>
       <NavLink
         className={styles.NavLink}
         to={`/profiles/${currentUser?.profile_id}`}
-      ><Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      >
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
       </NavLink>
       <Nav.Link className={styles.NavLink} to="/" onClick={handleSignOut}>
-      <i class="fa-solid fa-right-to-bracket fa-xl"></i>Sign out
+        <i class="fa-solid fa-right-to-bracket fa-xl"></i>Sign out
       </Nav.Link>
     </>
   );
@@ -59,11 +66,20 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <NavLink to="/">
         <Navbar.Brand className={styles.logo}>connectify.</Navbar.Brand>
       </NavLink>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle
+        ref={ref}
+        onClick={() => setExpanded(!expanded)}
+        aria-controls="basic-navbar-nav"
+      />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className=" ml-auto text-center">
           <NavLink className={styles.NavLink} to="/">
