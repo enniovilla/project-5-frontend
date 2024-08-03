ab4c1207bbdbfb818b807d8f3a5ae08d7ccf37f0
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 16:55:15 +0000

    Create event page

--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
+import Event from "./Event";
 
-function PostPage() {
-  // Add your logic here
+function EventPage() {
+  const { id } = useParams();
+  const [event, setEvent] = useState({ results: [] });
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: event }] = await Promise.all([
+          axiosReq.get(`/events/${id}`),
+        ]);
+        setEvent({ results: [event] });
+        console.log(event);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Event {...event.results[0]} setEvents={setEvent} eventPage />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
@@ -26,4 +43,4 @@ function PostPage() {
   );
 }
 
-export default PostPage;
+export default EventPage;
