b7f06e3fafe83092ff894a16e4a3b4b9305450e3
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 12:58:54 +0000

    Create axiosreq and res

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://connectify-api-da6695597009.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
