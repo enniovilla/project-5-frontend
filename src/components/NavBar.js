946977773b4f638e4fb833a69157cd0257d47579
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 13:28:45 +0000

    Add sign out handler

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,22 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
+import axios from "axios";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const loggedInIcons = (
     <>
@@ -30,7 +41,7 @@ const NavBar = () => {
         to={`/profiles/${currentUser?.profile_id}`}
       ><Avatar src={currentUser?.profile_image} text="Profile" height={40} />
       </NavLink>
-      <Nav.Link className={styles.NavLink}>
+      <Nav.Link className={styles.NavLink} to="/" onClick={handleSignOut}>
       <i class="fa-solid fa-right-to-bracket fa-xl"></i>Sign out
       </Nav.Link>
     </>
