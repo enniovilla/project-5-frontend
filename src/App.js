cb70d0374ededd0233f57f2aa7951d199202d2aa
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 12:42:53 +0000

    Import axios

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
+import "./api/axiosDefaults";
 
 function App() {
   return (
