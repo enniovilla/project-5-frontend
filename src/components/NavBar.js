462400f58b50801a53669fe06b3d8c3ddd5a07f8
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 13:47:05 +0000

    Change navlink styles

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -81,7 +81,7 @@ const NavBar = () => {
         aria-controls="basic-navbar-nav"
       />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className=" ml-auto text-center">
+        <Nav className="ml-auto text-center">
           <NavLink className={styles.NavLink} to="/">
             <i class="fa-solid fa-house fa-xl"></i>Home
           </NavLink>
