7da87645e6b486e7c035f62a858304d96c275bcd
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-03 13:07:45 +0000

    Create loggedin/out variables

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,24 @@ import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { useCurrentUser } from "../contexts/CurrentUserContext";
 
 const NavBar = () => {
+  const currentUser = useCurrentUser();
+
+  const loggedInIcons = <>{currentUser?.username}</>;
+
+  const loggedOutIcons = (
+    <>
+      <NavLink className={styles.NavLink} to="/signin">
+        <i class="fa-solid fa-right-to-bracket fa-xl"></i> Sign in
+      </NavLink>
+      <NavLink className={styles.NavLink} to="/signup">
+        <i class="fa-solid fa-user-pen fa-xl"></i> Sign up
+      </NavLink>
+    </>
+  );
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <NavLink to="/">
@@ -13,14 +29,9 @@ const NavBar = () => {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className=" ml-auto text-center">
           <NavLink className={styles.NavLink} to="/">
-          <i class="fa-solid fa-house fa-xl"></i> Home
+            <i class="fa-solid fa-house fa-xl"></i> Home
           </NavLink>
-          <NavLink className={styles.NavLink} to="/signin">
-          <i class="fa-solid fa-right-to-bracket fa-xl"></i> Sign in
-          </NavLink>
-          <NavLink className={styles.NavLink} to="/signup">
-          <i class="fa-solid fa-user-pen fa-xl"></i> Sign up
-          </NavLink>
+          {currentUser ? loggedInIcons : loggedOutIcons}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
