1c3ea9071fb4d566f6f833ba362e5834e196396b
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-04 21:55:03 +0000

    Add new routes

--- src/App.js
+++ src/App.js
@@ -12,6 +12,9 @@ import { useCurrentUser } from "./contexts/CurrentUserContext";
 import MyEvents from "./pages/events/MyEvents";
 import EventEditForm from "./pages/events/EventEditForm";
 import ProfilePage from "./pages/profiles/ProfilePage";
+import UsernameForm from "./pages/profiles/UsernameForm";
+import UserPasswordForm from "./pages/profiles/UserPasswordForm";
+import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 
 function App() {
   const currentUser = useCurrentUser();
@@ -62,8 +65,27 @@ function App() {
               />
             )}
           />
-          <Route exact path="/events/:id/edit" render={() => <EventEditForm />} />
+          <Route
+            exact
+            path="/events/:id/edit"
+            render={() => <EventEditForm />}
+          />
           <Route exact path="/profiles/:id" render={() => <ProfilePage />} />
+          <Route
+            exact
+            path="/profiles/:id/edit/username"
+            render={() => <UsernameForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit/password"
+            render={() => <UserPasswordForm />}
+          />
+          <Route
+            exact
+            path="/profiles/:id/edit"
+            render={() => <ProfileEditForm />}
+          />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
