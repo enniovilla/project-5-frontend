559dd59dcadffd45dc5ecb738382c6a4bdc59326
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-08-04 20:34:55 +0000

    Create profile component

--- src/pages/profiles/Profile.js
+++ src/pages/profiles/Profile.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import styles from "../../styles/Profile.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Link } from "react-router-dom";
+import Avatar from "../../components/Avatar";
+import { Button } from "react-bootstrap";
 
-const Profile = () => {
+const Profile = (props) => {
+  const { profile, imageSize = 55 } = props;
+  const { id, following_id, image, owner } = profile;
+
+  const currentUser = useCurrentUser();
+  const is_owner = currentUser?.username === owner;
+
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="my-3 d-flex align-items-center">
+      <div>
+        <Link className="align-self-center" to={`/profiles/${id}`}>
+          <Avatar src={image} height={imageSize} />
+        </Link>
+      </div>
+      <div className={`mx-2 ${styles.WordBreak}`}>
+        <strong>{owner}</strong>
+      </div>
+      <div className="text-right ml-auto">
+        {currentUser && !is_owner && (following_id ? (
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Color}`}
+            onClick={() => {}}
+          >
+            Unfollow
+          </Button>
+        ) : (
+          <Button
+            className={`${btnStyles.Button} ${btnStyles.Color}`}
+            onClick={() => {}}
+          >
+            Follow
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
